a35e96d3fe1517b8219007510d6aac434b51993e
Kahon7586 <kahon7586@gmail.com>
2024-02-06 19:30:14 +0800

    add: color theme of Dcard

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dcard: {
+          DEFAULT: "#006aa6",
+          light: "#3397cf",
+          "light-hover": "#5ab0db",
+          text: "#bfc7cc",
+          bg: "#00324e",
+          hover: "#002133",
+          select: "#4d7083",
+          border: "#005585",
+        },
+      },
+    },
   },
   plugins: [],
 }
