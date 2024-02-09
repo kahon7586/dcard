c7c712a4c12760ee5014235cb73771690540829c
Kahon7586 <kahon7586@gmail.com>
2024-02-09 10:21:19 +0800

    build: basic interface for MenuItem

---
+++ src/Data/Header.ts
@@ -0,0 +1,11 @@
+interface MenuItem {
+  content: string
+  path: string
+}
+
+export const menuItems: MenuItem[] = [
+  { content: "Policy", path: "/policy" },
+  { content: "Help Center", path: "/help-center" },
+  { content: "Contact Us", path: "/contact-us" },
+  { content: "Setting", path: "/setting" },
+]
