6341634c70da26fc7dbad840d11eca1764a1d601
Kahon <kahon7586@gmail.com>
2024-02-18 15:49:23 +0800

    build: basic and responsive structure

--- src/Layouts/LeftSideMenu.tsx
+++ src/Layouts/LeftSideMenu.tsx
@@ -1,17 +1,67 @@
-import React from "react"
-import Button from "../CVA/Button"
 import SideMenuSection from "../Components/SideMenu/SideMenuSection"
 import SideMenuItem from "../Components/SideMenu/SideMenuItem"
-import { generalShortCuts } from "../Data/SideMenu"
+import { generalShortCuts, trendingCategory, selectedCaregory } from "../Data/SideMenu"
+import { useEffect, useRef, useState } from "react"
 
 const LeftSideMenu = () => {
+  const scrollMenuRef = useRef<HTMLDivElement | null>(null)
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    const menu = scrollMenuRef.current
+    if (menu === null) return
+    const rect = menu.getBoundingClientRect()
+    setHeight(window.innerHeight - rect.top - 1)
+
+    function handler() {
+      console.log("detect resize, innerHeight: ", innerHeight)
+      setHeight(window.innerHeight - rect.top - 1)
+    }
+
+    window.addEventListener("resize", handler)
+
+    return () => {
+      window.removeEventListener("resize", handler)
+    }
+  }, [])
+
   return (
-    <aside>
+    <aside className="pt-6">
       <SideMenuSection>
-        {generalShortCuts.map((link) => (
-          <SideMenuItem />
+        {generalShortCuts.map((item) => (
+          <SideMenuItem
+            content={item.content}
+            Icon={item.Icon}
+            path={item.path}
+            key={item.path}
+          />
         ))}
       </SideMenuSection>
+      <div
+        className="overflow-y-auto"
+        ref={scrollMenuRef}
+        style={{ height: height }}>
+        <SideMenuSection title="Trending">
+          {trendingCategory.map((item) => (
+            <SideMenuItem
+              content={item.content}
+              Icon={item.Icon}
+              path={item.path}
+              key={item.path}
+            />
+          ))}
+        </SideMenuSection>
+        <SideMenuSection title="Just For You">
+          {selectedCaregory.map((item) => (
+            <SideMenuItem
+              content={item.content}
+              Icon={item.Icon}
+              path={item.path}
+              key={item.path}
+            />
+          ))}
+        </SideMenuSection>
+      </div>
     </aside>
   )
 }
