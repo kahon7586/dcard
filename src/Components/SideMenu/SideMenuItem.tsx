40ec1fd5a36e625f821e6224b122181c9131d4dc
Kahon <kahon7586@gmail.com>
2024-02-18 15:48:18 +0800

    build: basic structure

--- src/Components/SideMenu/SideMenuItem.tsx
+++ src/Components/SideMenu/SideMenuItem.tsx
@@ -1,15 +1,22 @@
-import React from "react"
 import { IconType } from "react-icons"
 import Button from "../../CVA/Button"
 
 interface SideMenuItemProps {
   content: string
-  icon: IconType
+  Icon: IconType
   path: string
 }
 
-const SideMenuItem = () => {
-  return <Button></Button>
+const SideMenuItem = ({ content, Icon, path }: SideMenuItemProps) => {
+  return (
+    <Button
+      className="flex items-center gap-4 font-bold text-lg w-[200px]"
+      variant="category"
+      size="default"
+      Icon={Icon}>
+      <a href={path}>{content}</a>
+    </Button>
+  )
 }
 
 export default SideMenuItem
--- src/Components/SideMenu/SideMenuSection.tsx
+++ src/Components/SideMenu/SideMenuSection.tsx
@@ -1,11 +1,17 @@
 import React, { ReactNode } from "react"
 
 interface SideMenuSectionProps {
+  title?: string
   children: ReactNode
 }
 
-const SideMenuSection = ({ children }: SideMenuSectionProps) => {
-  return <div></div>
+const SideMenuSection = ({ children, title }: SideMenuSectionProps) => {
+  return (
+    <section>
+      <div className="text-dcard-section-title px-3 font-bold py-1 cursor-default">{title}</div>
+      {children}
+    </section>
+  )
 }
 
 export default SideMenuSection
