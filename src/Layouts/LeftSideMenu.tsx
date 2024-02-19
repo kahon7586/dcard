a76d8f48fa3c8f00452b8361303b8491ecd0afb2
Kahon <kahon7586@gmail.com>
2024-02-19 09:06:52 +0800

    WIP: responsive behavior with different screen size

--- src/Layouts/LeftSideMenu.tsx
+++ src/Layouts/LeftSideMenu.tsx
@@ -2,10 +2,13 @@ import SideMenuSection from "../Components/SideMenu/SideMenuSection"
 import SideMenuItem from "../Components/SideMenu/SideMenuItem"
 import { generalShortCuts, trendingCategory, selectedCaregory } from "../Data/SideMenu"
 import { useEffect, useRef, useState } from "react"
+import Button from "../CVA/Button"
+import { IoIosArrowForward } from "react-icons/io"
 
 const LeftSideMenu = () => {
-  const scrollMenuRef = useRef<HTMLDivElement | null>(null)
   const [height, setHeight] = useState(0)
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false)
+  const scrollMenuRef = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
     const menu = scrollMenuRef.current
@@ -13,8 +16,15 @@ const LeftSideMenu = () => {
     const rect = menu.getBoundingClientRect()
     setHeight(window.innerHeight - rect.top - 1)
 
+    //fix sidemenu responsive
+
     function handler() {
-      console.log("detect resize, innerHeight: ", innerHeight)
+      const menu = scrollMenuRef.current
+      if (menu === null) return
+      const rect = menu.getBoundingClientRect()
+      if (window.innerWidth >= 768) {
+        setIsSideMenuOpen(false)
+      }
       setHeight(window.innerHeight - rect.top - 1)
     }
 
@@ -25,44 +35,69 @@ const LeftSideMenu = () => {
     }
   }, [])
 
+  function clickSideMenuBtn() {
+    setIsSideMenuOpen((prev) => !prev)
+  }
+
   return (
-    <aside className="pt-6">
-      <SideMenuSection>
-        {generalShortCuts.map((item) => (
-          <SideMenuItem
-            content={item.content}
-            Icon={item.Icon}
-            path={item.path}
-            key={item.path}
-          />
-        ))}
-      </SideMenuSection>
-      <div
-        className="overflow-y-auto"
-        ref={scrollMenuRef}
-        style={{ height: height }}>
-        <SideMenuSection title="Trending">
-          {trendingCategory.map((item) => (
-            <SideMenuItem
-              content={item.content}
-              Icon={item.Icon}
-              path={item.path}
-              key={item.path}
-            />
-          ))}
-        </SideMenuSection>
-        <SideMenuSection title="Just For You">
-          {selectedCaregory.map((item) => (
-            <SideMenuItem
-              content={item.content}
-              Icon={item.Icon}
-              path={item.path}
-              key={item.path}
-            />
-          ))}
-        </SideMenuSection>
+    <>
+      <div className={`${isSideMenuOpen ? "hidden" : "block"} md:hidden absolute left-[-12px] top-[50%]`}>
+        <Button
+          className="rounded-full hover:bg-dcard-hover"
+          variant="ghost"
+          size="icon"
+          onClick={clickSideMenuBtn}>
+          <IoIosArrowForward />
+        </Button>
       </div>
-    </aside>
+
+      <div
+        className={`${
+          isSideMenuOpen ? "block" : "hidden"
+        } md:hidden absolute top-0 bg-black bg-opacity-80 w-full h-full`}
+        onClick={() => setIsSideMenuOpen(false)}></div>
+
+      <aside
+        className={`bg-dcard-bg absolute top-0 left-0 ${
+          isSideMenuOpen ? "block" : "hidden"
+        } md:pt-6 md:block md:static`}>
+        <SideMenuSection>
+          {generalShortCuts.map((item) => (
+            <SideMenuItem
+              content={item.content}
+              Icon={item.Icon}
+              path={item.path}
+              key={item.path}
+            />
+          ))}
+        </SideMenuSection>
+        <div
+          className="overflow-y-auto"
+          ref={scrollMenuRef}
+          style={{ height: height }}>
+          <SideMenuSection title="Trending">
+            {trendingCategory.map((item) => (
+              <SideMenuItem
+                content={item.content}
+                Icon={item.Icon}
+                path={item.path}
+                key={item.path}
+              />
+            ))}
+          </SideMenuSection>
+          <SideMenuSection title="Just For You">
+            {selectedCaregory.map((item) => (
+              <SideMenuItem
+                content={item.content}
+                Icon={item.Icon}
+                path={item.path}
+                key={item.path}
+              />
+            ))}
+          </SideMenuSection>
+        </div>
+      </aside>
+    </>
   )
 }
 
