c12bc6d81b073475b84376bf648a5e853f38f9fc
Kahon <kahon7586@gmail.com>
2024-02-23 18:05:50 +0800

    change: trun the resize listener into custom hook, use external placeholder and data as postboard img and post data; add: sticky behavior

--- src/Layouts/PostBoard.tsx
+++ src/Layouts/PostBoard.tsx
@@ -1,44 +1,37 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react"
-import postImg from "../Assets/1280.webp"
+import { useLayoutEffect, useRef, useState } from "react"
 import Button from "../CVA/Button"
 import { MoodBoardInfo } from "../Data/MoodBoardInfo"
 import Post from "../Components/Postboard/Post"
-import { PostListData } from "../Data/PostListData"
 import { setHeightToBottom } from "../Lib/setHeightToBottom"
+import { useReachBottom } from "../Hooks/useReachBottom"
+import { usePostList } from "../Hooks/usePostList"
 
 const Postboard = () => {
   const { name, Icon } = MoodBoardInfo
   const LABELS: string[] = ["Hot", "New", "Rules"]
 
   const [currLabel, setCurrLabel] = useState(LABELS[0])
+
   const PostboardRef = useRef<HTMLDivElement | null>(null)
 
   useLayoutEffect(() => {
     setHeightToBottom(PostboardRef)
   }, [])
 
-  useEffect(() => {
-    function handler() {
-      setHeightToBottom(PostboardRef)
-    }
+  useReachBottom(PostboardRef)
 
-    window.addEventListener("resize", handler)
-
-    return () => {
-      window.removeEventListener("resize", handler)
-    }
-  }, [])
+  const [isPostsLoad, postList] = usePostList()
 
   return (
     <div
-      className="flex bg-white flex-col min-w-[500px] max-w-[768px] mt-2 mx-2 overflow-y-auto"
+      className="flex  bg-white flex-col min-w-[500px] max-w-[768px] mt-2 mx-2  overflow-y-auto"
       ref={PostboardRef}>
       <img
         className="bg-black object-cover object-bottom"
-        src={postImg}
+        src={"https://placehold.co/1280x427"}
       />
 
-      <div className="flex flex-col bg-white px-8 py-8">
+      <div className="flex flex-col bg-white px-8 pt-8 mb-8 sticky top-0">
         <div className="flex justify-between items-center pb-4">
           <div className="flex gap-6 text-[2rem] items-center">
             <Icon />
@@ -61,15 +54,19 @@ const Postboard = () => {
         </div>
 
         <hr />
+      </div>
 
-        <div>
-          {PostListData.map((post) => (
+      <div className="px-8">
+        {isPostsLoad ? (
+          postList?.posts.map((post) => (
             <Post
-              key={post.postId}
-              postId={post.postId}
+              post={post}
+              key={post.id}
             />
-          ))}
-        </div>
+          ))
+        ) : (
+          <p>loading</p>
+        )}
       </div>
     </div>
   )
