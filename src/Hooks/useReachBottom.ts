074ba4af1c9f82f34f2a54cc303f138c076b09c2
Kahon <kahon7586@gmail.com>
2024-02-23 18:06:20 +0800

    build: resize listener to set ref element height to viewport bottom

---
+++ src/Hooks/useReachBottom.ts
@@ -0,0 +1,20 @@
+import { MutableRefObject, useEffect } from "react"
+import { setHeightToBottom } from "../Lib/setHeightToBottom"
+
+// add a resize listener to ref element that adjust height to reach viewport bottom,
+// this hook also accept callbackFn to operate when listener is triggered
+
+export function useReachBottom(divRef: MutableRefObject<HTMLDivElement | null>, callbackFn?: () => void) {
+  useEffect(() => {
+    function handler() {
+      setHeightToBottom(divRef)
+      if (callbackFn !== undefined) callbackFn()
+    }
+
+    window.addEventListener("resize", handler)
+
+    return () => {
+      window.removeEventListener("resize", handler)
+    }
+  }, [])
+}
