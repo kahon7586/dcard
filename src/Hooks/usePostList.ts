a6d1914de9ef385aa7a75e6335b30ac587ca1978
Kahon <kahon7586@gmail.com>
2024-02-24 16:50:58 +0800

    fix: qurey type check; WIP : post load checker

--- src/Hooks/usePostList.ts
+++ src/Hooks/usePostList.ts
@@ -19,63 +19,65 @@ export interface PostList {
 }
 
 export interface PostQuery {
-  limit?: number
-  skip?: number
-  delay?: number
-  select?: string[]
+  limit: number
+  skip: number
+  delay: number
+  select: string[]
 }
 
-function createPostQueryUrl(options: Partial<PostQuery>) {
+function createPostQueryUrl(options: PostQuery) {
   // exampleUrl https://dummyjson.com/posts?limit=10&skip=10&select=title,reactions,userId
   const basic = "https://dummyjson.com/posts"
   const queryPairs = Object.entries(options) as [string, number | string[]][] // [[name, value], [name, value], ...]
   let queryUrl = ""
 
   queryPairs.forEach(([name, value]) => {
-    console.log(name, value)
-    if (value === 0 || []) return
-    const heading = queryUrl.length === 0 ? "?" : "&"
-    if (typeof value === "number") queryUrl += heading + name + "=" + value
-    else queryUrl += heading + name + "=" + value.join(",")
-  })
+    if (value === 0 || (value instanceof Array && value.length === 0)) return
+    // skip when no value
 
-  console.log("queryUrl: " + basic + queryUrl)
+    const heading = queryUrl.length === 0 ? "?" : "&"
+
+    if (typeof value === "number") {
+      queryUrl += heading + name + "=" + value
+    } else {
+      queryUrl += heading + name + "=" + value.join(",")
+    }
+  })
 
   return basic + queryUrl
 }
 
-export function usePostList(options: PostQuery) {
+export function usePostList(options: Partial<PostQuery>, dep: React.DependencyList) {
   const DEFAULT_VALUE: PostQuery = { limit: 0, skip: 0, delay: 0, select: [] }
-  const mergeValue: PostQuery = { ...DEFAULT_VALUE, ...options }
-
-  console.log("query params:")
-  console.log(mergeValue)
-
-  const postUrl = createPostQueryUrl(mergeValue)
+  const mergedValue: PostQuery = { ...DEFAULT_VALUE, ...options }
 
   const [isPostsLoad, setIsPostLoad] = useState(false)
   const [postList, setPostList] = useState<PostList | null>(null)
 
   useEffect(() => {
+    const abortController = new AbortController()
+    const postUrl = createPostQueryUrl(mergedValue)
+
     async function fetchPostData() {
+      console.log("fetching post data: " + postUrl)
       try {
-        let res = await fetch(postUrl)
+        let res = await fetch(postUrl, { signal: abortController.signal })
         let data = (await res.json()) as PostList // structure should follow postList
-        console.log(data)
+        console.log("post fetching complete!")
         setPostList(data)
+        setIsPostLoad(true)
       } catch (err) {
         console.log(err)
+        setIsPostLoad(true)
       }
     }
 
-    // fetchPostData()
-  }, [])
+    fetchPostData()
 
-  useEffect(() => {
-    if (postList !== null) {
-      setIsPostLoad(true)
+    return () => {
+      abortController.abort()
     }
-  }, [postList])
+  }, dep)
 
   return [isPostsLoad, postList] as [boolean, PostList | null]
 }
