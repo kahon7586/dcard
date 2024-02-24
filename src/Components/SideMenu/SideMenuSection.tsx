f7cdfeae67c755308cc4448726f4472282f43825
Kahon <kahon7586@gmail.com>
2024-02-24 10:39:53 +0800

    fix: hide title section px when no content

--- src/Components/SideMenu/SideMenuSection.tsx
+++ src/Components/SideMenu/SideMenuSection.tsx
@@ -8,7 +8,10 @@ interface SideMenuSectionProps {
 const SideMenuSection = ({ children, title }: SideMenuSectionProps) => {
   return (
     <section>
-      <div className="text-dcard-section-title px-3 font-bold py-2 cursor-default">{title}</div>
+      <div
+        className={`text-dcard-section-title px-3 font-bold ${title === undefined ? "py-0" : "py-2"} cursor-default`}>
+        {title}
+      </div>
       {children}
     </section>
   )
