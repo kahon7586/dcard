840b40b854a8d166621f7df0d0cb1b9f8fd436ea
Kahon <kahon7586@gmail.com>
2024-02-24 12:47:17 +0800

    add: interface to control query value

--- src/Hooks/usePostList.ts
+++ src/Hooks/usePostList.ts
@@ -18,8 +18,40 @@ export interface PostList {
   limit: 30
 }
 
-export function usePostList() {
-  const postUrl = "https://dummyjson.com/posts?delay=2000&limit=10&skip=10"
+export interface PostQuery {
+  limit?: number
+  skip?: number
+  delay?: number
+  select?: string[]
+}
+
+function createPostQueryUrl(options: Partial<PostQuery>) {
+  // exampleUrl https://dummyjson.com/posts?limit=10&skip=10&select=title,reactions,userId
+  const basic = "https://dummyjson.com/posts"
+  const queryPairs = Object.entries(options) as [string, number | string[]][] // [[name, value], [name, value], ...]
+  let queryUrl = ""
+
+  queryPairs.forEach(([name, value]) => {
+    console.log(name, value)
+    if (value === 0 || []) return
+    const heading = queryUrl.length === 0 ? "?" : "&"
+    if (typeof value === "number") queryUrl += heading + name + "=" + value
+    else queryUrl += heading + name + "=" + value.join(",")
+  })
+
+  console.log("queryUrl: " + basic + queryUrl)
+
+  return basic + queryUrl
+}
+
+export function usePostList(options: PostQuery) {
+  const DEFAULT_VALUE: PostQuery = { limit: 0, skip: 0, delay: 0, select: [] }
+  const mergeValue: PostQuery = { ...DEFAULT_VALUE, ...options }
+
+  console.log("query params:")
+  console.log(mergeValue)
+
+  const postUrl = createPostQueryUrl(mergeValue)
 
   const [isPostsLoad, setIsPostLoad] = useState(false)
   const [postList, setPostList] = useState<PostList | null>(null)
@@ -29,14 +61,14 @@ export function usePostList() {
       try {
         let res = await fetch(postUrl)
         let data = (await res.json()) as PostList // structure should follow postList
-        console.log("data fetched")
         console.log(data)
         setPostList(data)
       } catch (err) {
         console.log(err)
       }
     }
-    fetchPostData()
+
+    // fetchPostData()
   }, [])
 
   useEffect(() => {
