3147c833b13646d2aff889ded329f4db5662ad4a
Kahon <kahon7586@gmail.com>
2024-02-27 16:35:46 +0800

    add: Test

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
+import Test from "./Test.tsx"
 
 const STRICT_MODE = true
 
 if (STRICT_MODE) {
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-      <App />
+      {/* <App /> */}
+      <Test />
     </React.StrictMode>
   )
 } else {
