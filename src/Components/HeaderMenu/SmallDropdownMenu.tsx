78333f937b008970fc53d66e7a0d842f69a89c72
Kahon <kahon7586@gmail.com>
2024-02-29 17:54:50 +0800

    rename: SmallHeader Menu -> SmallDropdownMenu

--- src/Components/HeaderMenu/SmallDropdownMenu.tsx
+++ src/Components/HeaderMenu/SmallDropdownMenu.tsx
@@ -1,21 +1,20 @@
-import { useEffect } from "react"
 import { useHeaderMenu } from "../../Context/useHeaderMenu"
 import { menuItems } from "../../Data/Header"
 import Button from "../../CVA/Button"
 
-interface SmallHeaderMenuProps {
+interface SmallDropdownMenuProps {
   isSmallScreen: boolean
 }
 
-const SmallHeaderMenu = ({ isSmallScreen }: SmallHeaderMenuProps) => {
-  const { isToggled, close } = useHeaderMenu()
+const SmallDropdownMenu = ({ isSmallScreen }: SmallDropdownMenuProps) => {
+  const { isToggled } = useHeaderMenu()
 
   return (
     <ul
-      className={`absolute border rounded-lg bg-white text-nowrap right-0 lg:right-[103px] top-[45px] ${
+      className={`absolute z-[98] border shadow-lg rounded-lg bg-white text-nowrap right-0 lg:right-[103px] top-[45px] ${
         isToggled ? "block" : "hidden"
       }`}>
-      <div className="absolute top-[-10px] right-[6px] w-0 h-0 border-x-[10px] border-solid border-x-transparent border-b-[10px] border-b-white" />
+      <div className="absolute top-[-9px] z-97 right-[6px] w-0 h-0 border-x-[10px] border-solid border-x-transparent border-b-[10px] border-b-white" />
       {isSmallScreen && (
         <li className="hover:bg-dcard-text rounded-t-lg px-4 py-2 flex">
           <a href="/login">Login/Register</a>
@@ -39,4 +38,4 @@ const SmallHeaderMenu = ({ isSmallScreen }: SmallHeaderMenuProps) => {
   )
 }
 
-export default SmallHeaderMenu
+export default SmallDropdownMenu
