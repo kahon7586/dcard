c8cff3783c3a734c85ed5b5c3933711e342209f7
Kahon <kahon7586@gmail.com>
2024-02-29 18:19:05 +0800

    remove: unnecessary context

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import Header from "./Layouts/Header"
 import "./App.css"
-import MyContextProvider from "./Context/MyContextProvider"
 import LeftSideMenu from "./Layouts/LeftSideMenu"
-import Postboard from "./Layouts/Postboard"
 import RightSidebar from "./Layouts/RightSidebar"
 import { MoodBoardInfo } from "./Data/MoodBoardInfo"
+import Postboard from "./Layouts/Postboard"
 
 const App = () => {
   return (
-    <MyContextProvider>
+    <>
       <Header />
       <div className="flex justify-center items-start mx-auto">
         <LeftSideMenu />
         <Postboard />
         <RightSidebar boardInfo={MoodBoardInfo} />
       </div>
-    </MyContextProvider>
+    </>
   )
 }
 
