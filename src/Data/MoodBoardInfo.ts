b33457be95491e759a4a4d77825f6929de8f86f1
Kahon <kahon7586@gmail.com>
2024-02-29 14:57:26 +0800

    trivial: change to a proper name

--- src/Data/MoodBoardInfo.ts
+++ src/Data/MoodBoardInfo.ts
@@ -1,7 +1,7 @@
 import { BsCupHot } from "react-icons/bs"
-import { BoardInfo } from "./BoardInfo"
+import { PostboardInfo } from "./PostboardInfo"
 
-export const MoodBoardInfo: BoardInfo = {
+export const MoodBoardInfo: PostboardInfo = {
   name: "Mood",
   info: "A platform where individuals can freely share their emotions, experiences, and stories anonymously. It emphasizes stress-free writing and encourages discussions on various life topics. However, it directs political discussions to a separate board to maintain focus on personal narratives.",
   Icon: BsCupHot,
