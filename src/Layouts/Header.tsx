e577726babfee6ba6bfaf917c307b7fb22825ea8
Kahon <kahon7586@gmail.com>
2024-02-29 18:09:06 +0800

    build: split the part contains hamburger, dropdown menu, login/register and get app; remove: screen size condition rendering

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,12 +1,8 @@
 import { FaSearch } from "react-icons/fa"
 import Dcard_logo from "../Assets/logo.8b5bbef2.svg"
-import { IoMdMenu as Hamburger } from "react-icons/io"
 import Button from "../CVA/Button"
-import { VscTriangleDown, VscTriangleUp } from "react-icons/vsc"
-import { useEffect, useRef, useState } from "react"
-import SmallHeaderMenu from "../Components/HeaderMenu/SmallHeaderMenu"
-import { useHeaderMenu } from "../Context/useHeaderMenu"
 import React from "react"
+import HeaderRightSetions from "../Components/HeaderMenu/HeaderRightSetions"
 
 const Logo = React.memo(() => {
   return (
@@ -22,33 +18,8 @@ const Logo = React.memo(() => {
 })
 
 const Header = () => {
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 1024)
-
-  const { isToggled, toggle } = useHeaderMenu()
-  const headerRef = useRef<HTMLHeadElement>(null)
-
-  useEffect(() => {
-    if (headerRef.current === null) return
-
-    function handleResize() {
-      if (window.innerWidth < 1024) {
-        setIsSmallScreen(true)
-      } else {
-        setIsSmallScreen(false)
-      }
-    }
-
-    const headerObserver = new ResizeObserver(handleResize)
-
-    headerObserver.observe(headerRef.current)
-
-    return () => headerObserver.disconnect()
-  }, [])
-
   return (
-    <header
-      className="bg-dcard "
-      ref={headerRef}>
+    <header className="bg-dcard ">
       <div className=" gap-6 mx-auto max-w-[1024px] flex px-16 py-2.5 lg:justify-center justify-around items-center">
         <Logo />
 
@@ -66,33 +37,7 @@ const Header = () => {
           </Button>
         </div>
 
-        <div>
-          <div className="relative">
-            <Button
-              className="lg:hidden flex size-8 hover:bg-dcard-light rounded-md"
-              variant="ghost"
-              size="icon"
-              onClick={toggle}>
-              <Hamburger className="h-full w-full p-1" />
-            </Button>
-            <SmallHeaderMenu isSmallScreen={isSmallScreen} />
-          </div>
-
-          <div className="lg:flex hidden gap-4">
-            <Button
-              className="text-white px-0"
-              variant="ghost">
-              <a href="/login">Login/Register</a>
-            </Button>
-            <Button
-              size="icon"
-              variant="ghost"
-              onClick={toggle}>
-              {isToggled ? <VscTriangleDown fill="white" /> : <VscTriangleUp fill="white" />}
-            </Button>
-            <Button className="font-bold">Get App</Button>
-          </div>
-        </div>
+        <HeaderRightSetions />
       </div>
     </header>
   )
