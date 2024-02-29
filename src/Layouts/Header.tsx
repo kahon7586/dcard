afe764dd297137d9caf355f579a17c2fab2a2b20
Kahon <kahon7586@gmail.com>
2024-02-29 18:11:42 +0800

    change: split searchBar

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -17,25 +17,31 @@ const Logo = React.memo(() => {
   )
 })
 
+const SearchBar = () => {
+  return (
+    <div className=" max-w-[500px] flex border border-dcard-border rounded-md flex-grow">
+      <input
+        className=" flex-grow rounded-l-md bg-dcard-border px-2 placeholder-dcard-text placeholder-opacity-50"
+        type="text"
+        placeholder="Search..."
+      />
+      <Button
+        size="icon"
+        variant="ghost"
+        className="rounded-r-md flex-grow-0">
+        <FaSearch fill="white" />
+      </Button>
+    </div>
+  )
+}
+
 const Header = () => {
   return (
     <header className="bg-dcard ">
       <div className=" gap-6 mx-auto max-w-[1024px] flex px-16 py-2.5 lg:justify-center justify-around items-center">
         <Logo />
 
-        <div className=" max-w-[500px] flex border border-dcard-border rounded-md flex-grow">
-          <input
-            className=" flex-grow rounded-l-md bg-dcard-border px-2 placeholder-dcard-text placeholder-opacity-50"
-            type="text"
-            placeholder="Search..."
-          />
-          <Button
-            size="icon"
-            variant="ghost"
-            className="rounded-r-md flex-grow-0">
-            <FaSearch fill="white" />
-          </Button>
-        </div>
+        <SearchBar />
 
         <HeaderRightSetions />
       </div>
