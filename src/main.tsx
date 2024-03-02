1b1ee2a2d81e70deb089dc3004a81e472fc71d7b
Kahon <kahon7586@gmail.com>
2024-03-02 11:07:38 +0800

    remove: testing

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
-import Test from "./Test.tsx"
 
-const STRICT_MODE = true
-
-if (STRICT_MODE) {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <App />
-      {/* <Test /> */}
-    </React.StrictMode>
-  )
-} else {
-  ReactDOM.createRoot(document.getElementById("root")!).render(<App />)
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
